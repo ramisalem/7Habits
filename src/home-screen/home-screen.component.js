1a24e1de3d7d7e57837f4238bf89e81ab7f89a93
Allen Yang <Shentao.Yang@outlook.com>
2018-08-03 11:43:26 +0800

    Remove unused files

--- src/home-screen/home-screen.component.js
+++ src/home-screen/home-screen.component.js
@@ -6,30 +6,19 @@ import {
     Image
 } from "react-native";
 
-//library imports
 import { Icon, Button, Container, Header, Content, Left } from 'native-base'
 
-//custom components imports
-import CustomHeader from './CustomHeader'
 
 export class HomeScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
         title: "Home",
         headerLeft: <Icon name="ios-menu" style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} />,
-        drawerLabel: 'Notification',
-        drawerIcon: ({ tintColor }) => (
-            <Image
-                source={require('./DrawerIcons/home.png')}
-                style={styles.icon}
-            />
-        ),
-    })
+    });
 
 
     render() {
         return (
             <Container>
-                {/*<CustomHeader title="Home2" drawerOpen={() => this.props.navigation.openDrawer()} />*/}
                 <Content
                     contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10 }}>
                     <Button
