77451ae65ddaa96bdfaa894d60d34e375a7f0931
Allen Yang <Shentao.Yang@outlook.com>
2018-08-06 14:27:13 +0800

    Refactor home screen

--- src/home-screen/home-screen.component.js
+++ src/home-screen/home-screen.component.js
@@ -6,38 +6,38 @@ import {
     Image
 } from "react-native";
 
-import { Icon, Button, Container, Header, Content, Left } from 'native-base'
+import { Icon } from 'native-base'
 
+const menuIcon = (navigation) => (
+    <Icon
+        name="ios-menu"
+        style={styles.icon}
+        onPress={() => navigation.openDrawer()}
+    />
+);
 
 export class HomeScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
         title: "Home",
-        headerLeft: <Icon name="ios-menu" style={{ paddingLeft: 10 }} onPress={() => navigation.openDrawer()} />,
+        headerLeft: menuIcon(navigation),
     });
-
 
     render() {
         return (
-            <Container>
-                <Content
-                    contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10 }}>
-                    <Button
-                        onPress={() => this.props.navigation.navigate('Settings')} full>
-                        <Text style={{ color: 'white' }}>Go To Settings Screen</Text>
-                    </Button>
-                </Content>
+            <View style={styles.container}>
 
-            </Container>
-
+            </View>
         )
     }
-
 }
 
 
 const styles = StyleSheet.create({
     icon: {
-        width: 24,
-        height: 24,
+        paddingLeft: 10,
     },
+    container: {
+        flex: 1,
+        backgroundColor: '#F7F7F7',
+    }
 });
