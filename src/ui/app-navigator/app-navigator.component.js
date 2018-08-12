abe6723d124b3e1eba09c2c4f0e0f7a5232a6f6e
Allen Yang <Shentao.Yang@outlook.com>
2018-08-12 14:04:22 +0800

    Finally was able to show the drawer icon and label :)

--- src/ui/app-navigator/app-navigator.component.js
+++ src/ui/app-navigator/app-navigator.component.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { stackNavigatorConfig } from "./config/stack-navigator-config";
 import { drawerNavigatorConfig } from "./config/drawer-navigator-config";
+
+import { Color } from "../shared/colors";
 
 import {
     BeProactiveScreen,
@@ -32,10 +35,29 @@ const FirstThingFirstStackRoutes = {
     }
 };
 
+export const getDrawerNavigationOptions = (title, backgroundColor, titleColor, drawerIcon) => ({
+    title,
+    headerTitle: title,
+    headerStyle: {
+        backgroundColor,
+    },
+    headerTitleStyle: {
+        color: titleColor,
+    },
+    headerTintColor: titleColor,
+    drawerLabel: title,
+    drawerIcon,
+});
+
+const getDrawerIcon = (iconName, tintColor) => <Icon name={iconName} size={20} color={tintColor} />;
+const homeDrawerIcon = ({ tintColor }) => getDrawerIcon('home', tintColor);
+const homeNavOptions = getDrawerNavigationOptions('Home', Color.Banana, 'white', homeDrawerIcon);
+
 const DrawerRoutes = {
     BeProactive: {
         name: 'BeProactiveName',
-        screen: createStackNavigator(BeProactiveStackRoutes, { initialRouteName: 'BeProactiveScreen', ...stackNavigatorConfig })
+        screen: createStackNavigator(BeProactiveStackRoutes, { initialRouteName: 'BeProactiveScreen', ...stackNavigatorConfig}),
+        navigationOptions: homeNavOptions,
     },
     BeginWithEnd: {
         name: 'BeginWithEndName',
@@ -47,14 +69,16 @@ const DrawerRoutes = {
     },
 };
 
+const drawer = createDrawerNavigator(
+    DrawerRoutes,
+    drawerNavigatorConfig
+);
+
 export const RootNavigator =
     createStackNavigator({
             Drawer: {
                 name: 'Drawer',
-                screen: createDrawerNavigator(
-                    DrawerRoutes,
-                    drawerNavigatorConfig
-                ),
+                screen: drawer,
             },
             // ...Stack
         },
